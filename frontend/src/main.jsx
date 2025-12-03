df0f0c1f3ea306859c10c3cf9e60b85d93453c35
Sobhan K_G <Sobhan_Alipour@gmail.com>
2025-12-03 18:42:08 +0330

    Change front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/custom_bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css'

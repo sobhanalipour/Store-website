3293f8793a22c7eb26f05f50a8ebdea04121cd36
Sobhan K_G <Sobhan_Alipour@gmail.com>
2025-12-03 23:23:14 +0330

    add icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Navber from './components/Navbar/Navbar';
 import SearchBox from './components/Searchbox/Searchbox';
 import Carousel from './components/Carousel/Carousel';
-import supermarketlogo from "./assets/image/CategoryLogos/SuperMarketLogo/supermarket_logo.png/"
+import supermarketlogo from "./assets/image/CategoryLogos/SuperMarketLogo/icons8-supermarket-96.png/"
 import behdashtylogo from "./assets/image/CategoryLogos/BehdashtyLogo/behdashty_logo.png/"
 import tahrirlogo from "./assets/image/CategoryLogos/TahrirLogo/Tahrir_logo.png"
 import janebilogo from "./assets/image/CategoryLogos/JanebiLogo/janebi_logo.png/"
@@ -37,7 +37,7 @@ function App() {
           <div className='col-md-3 col-6'>
             <img className='img-fluid' src={supermarketlogo} alt="" />
             <a className='text-dark' href="">
-              <h5>سورمارکت</h5>
+              <h5>سوپرمارکت</h5>
             </a>
           </div>
           <div className='col-md-3 col-6'>
